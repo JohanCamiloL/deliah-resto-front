ab6c0100739526e763df930b49d2b50dc97cd8aa
Camilo Lopez <johan.lopez@bluetab.net>
2020-08-03 22:15:12 -0500

    Some static functionality added

---
+++ js/index-script.js
@@ -0,0 +1,3 @@
+document.getElementById('form-submit').addEventListener('click', () => {
+    window.location.href = 'dishes.html';
+});
--- js/order-script.js
+++ js/order-script.js
@@ -1,3 +1,3 @@
 document.getElementById('payment-order-submit').addEventListener('click', () => {
-    window.location.href = './follow-order.html';
+    window.location.href = './order-received.html';
 })
